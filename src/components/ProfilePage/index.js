4337d6de397909dd9ad16752062521ad1e3986f5
Derick Quartey <niikwartey1@gmail.com>
2017-05-13 21:54:59 -0400

    fix git fuckup

---
+++ src/components/ProfilePage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const ProfilePage = () => {
+  return (
+    <div className="ProfilePage">
+      <h1>ProfilePage</h1>
+    </div>
+  );
+}
+
+export default ProfilePage;
