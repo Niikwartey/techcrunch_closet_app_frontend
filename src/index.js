8a03c107d183ca2e1337e325a94e9a29af9a5956
Derick Quartey <niikwartey1@gmail.com>
2017-05-13 22:06:47 -0400

    add redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import routes from './routes';
+import store from './store';
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter children={routes} />,
+  <Provider store={store}>
+    <BrowserRouter children={routes} />
+  </Provider>,
   document.getElementById('root')
 );
